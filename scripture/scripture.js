3376c7c9c822834b842da80a15ef501b9f3d9b61
Jordan Shurmer <jordan@shurmer.family>
2019-04-30 06:46:50 -0400

    Add open by default for scripture-passage

--- scripture/scripture.js
+++ scripture/scripture.js
@@ -34,17 +34,15 @@
     <style>
         :host {
             display: block;
-            position: relative;
         }
         :host[hidden] {
             display: none;
         }
 
         :host #reference::before {
-            content: '⏵';
+            content: '❯';
             display: inline-block;
             width: 25px;
-            margin-bottom: 1em;
         }
         :host([open]) #reference::before {
             content: '⏷';
@@ -81,18 +79,21 @@
             this.attachShadow({mode: 'open'});
             this.shadowRoot.appendChild(template.content.cloneNode(true));
 
+            if (this.hasAttribute("api")) {
+                this.api = eval(this.getAttribute("api"));
+            }
+
             this.passages = undefined;
+            if (this.open) {
+                this._query();
+            }
             console.groupEnd();
         }
 
         connectedCallback() {
             console.group("ScripturePassage ConnectedCallback:");
 
-
             console.debug("api attribute:", this.getAttribute("api"))
-            if (this.hasAttribute("api")) {
-                this.api = eval(this.getAttribute("api"));
-            }
             this.addEventListener('mousedown', this._mouseDown);
             console.groupEnd();
         }
@@ -102,6 +103,12 @@
         }
 
 
+        _query() {
+            return this.api.query(this.reference)
+                .then(passages => {
+                    this.passages = passages;
+                });
+        }
 
         _mouseDown(event) {
             if (!this.api) return; //maybe toggle the .open??
@@ -111,12 +118,7 @@
                 this.open = !this.open;
             } else {
                 if ('SUMMARY' === event.path[0].tagName) {
-                    this.api.query(this.reference)
-                        .then(passages => {
-                            this.passages = passages;
-                            console.debug("toggling [open]");
-                            this.open = !this.open;
-                        });
+                    this._query().then(() => this.open = !this.open);
                 }
             }
             console.groupEnd();
